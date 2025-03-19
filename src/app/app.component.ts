f10565eab475f6a32e82212a1c6d1f89c7ff6530
Breno Ramos <145413402+brsg05@users.noreply.github.com>
2025-03-18 23:56:25 -0300

    Atualizando o component menu-title.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SmallCardComponent, BigCardComponent],
+  imports: [RouterOutlet, SmallCardComponent, BigCardComponent, MenuBarComponent, MenuTitleComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/components/menu-title/menu-title.component.ts
+++ src/app/components/menu-title/menu-title.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-title',
-  imports: [],
+  //imports: [],
   templateUrl: './menu-title.component.html',
-  styleUrl: './menu-title.component.css'
+  styleUrls: ['./menu-title.component.css']
 })
-export class MenuTitleComponent {
+
+export class MenuTitleComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
