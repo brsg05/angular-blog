442adcd1aa2fb4b0d6cfc78410452940f32671b2
Breno Ramos <145413402+brsg05@users.noreply.github.com>
2025-03-18 23:39:28 -0300

    Testando e ajustando o básico de components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SmallCardComponent, BigCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular-blog';
---
+++ src/app/components/big-card/big-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-big-card',
+  //imports: [],
+  templateUrl: './big-card.component.html',
+  styleUrls: ['./big-card.component.css']
+})
+export class BigCardComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
